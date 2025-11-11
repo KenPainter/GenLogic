f8d6c54f2d2c6489484778e3778cb6adead2a162
Ken Downs <kendowns@protonmail.com>
2025-11-10 22:32:09 -0500

    still removing dead code

--- src/processor.ts
+++ src/processor.ts
@@ -1,12 +1,10 @@
 import { writeFileSync } from 'fs';
 import { dirname, basename, extname, join } from 'path';
 import type { DatabaseConfig } from './types.js';
-import { DataFlowGraphValidator } from './graph.js';
 import { SchemaProcessor } from './schema-processor.js';
 import { DatabaseManager } from './database.js';
 import { DiffEngine, type SchemaDiff } from './diff-engine.js';
 import { SQLGenerator, type SQLStatements } from './sql-generator.js';
-import { TriggerGenerator } from './trigger-generator.js';
 import { ContentManager } from './content-manager.js';
 import { ResolvedSchemaGenerator } from './resolved-schema-generator.js';
 import { PermissionsGenerator } from './permissions-generator.js';
@@ -36,12 +34,10 @@ import { generateTriggersDDL } from './helpers-ddl/triggers.js';
  */
 export class GenLogicProcessor {
   private config: DatabaseConfig;
-  private graphValidator: DataFlowGraphValidator;
   private schemaProcessor: SchemaProcessor;
   private database: DatabaseManager;
   private diffEngine: DiffEngine;
   private sqlGenerator: SQLGenerator;
-  private triggerGenerator: TriggerGenerator;
   private contentManager: ContentManager;
   private resolvedSchemaGenerator: ResolvedSchemaGenerator;
   private permissionsGenerator: PermissionsGenerator;
@@ -49,12 +45,10 @@ export class GenLogicProcessor {
 
   constructor(config: DatabaseConfig) {
     this.config = config;
-    this.graphValidator = new DataFlowGraphValidator();
     this.schemaProcessor = new SchemaProcessor();
     this.database = new DatabaseManager(config);
     this.diffEngine = new DiffEngine();
     this.sqlGenerator = new SQLGenerator();
-    this.triggerGenerator = new TriggerGenerator();
     this.contentManager = new ContentManager();
     this.resolvedSchemaGenerator = new ResolvedSchemaGenerator();
     this.permissionsGenerator = new PermissionsGenerator();
